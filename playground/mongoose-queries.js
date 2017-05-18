d9bef0e06c5676fc31f5daeb90f45427e08020c4
Erik Appelblad <et08ead@cs.umu.se>
2017-05-18 10:28:44 +0200

    Add mongoose-queries in playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,40 @@
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+const {ObjectID} = require('mongodb');
+
+var id = "591c9ff4ebb7369c124c4e9c";
+
+User.findById(id).then((user) => {
+  if(!user) {
+    return console.log('User not found');
+  }
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
+
+// var id = "591d511712eaa570299c7dec";
+//
+// if(!ObjectID.isValid(id)) {
+//   console.log('Id not valid');
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if(!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo by id', todo);
+// }).catch((e) => console.log(e));
